e5b7ddee0cb91e8e87735985d935555e396dc8e5
Praveen Verma <knowitspraveeno@gmail.com>
2025-12-16 23:28:52 +0530

    Simple Counting

--- src/LeetcodeMedium/Leetcode1497.java
+++ src/LeetcodeMedium/Leetcode1497.java
@@ -2,29 +2,24 @@ package LeetcodeMedium;
 
 public class Leetcode1497 {
     public static void main(String[] args) {
-        int[] arr = { -1,-1,-1,-1,2,2,-2,-2};
-        int k = 10;
+        int[] arr = {1,2,3,4,5,10,6,7,8,9};
+        int k = 5;
          boolean ans = findit(arr,k);
         System.out.println(ans);
     }
     static boolean findit(int[] arr, int k){
-        int count = 0;
+        int[] freq = new int[k+1];
+
         for(int i = 0; i<arr.length; i++){
-            for(int j = i+1; j<arr.length; j++){
-                if(arr[i] == Integer.MIN_VALUE || arr[j] == Integer.MIN_VALUE){
-                    continue;
-                }
-                int sum = (arr[i]+arr[j]);
-                if(sum%k == 0){
-                    count++;
-                    arr[j] = Integer.MIN_VALUE;
-                    break;
-                }
+            int r = (arr[i]%k+k)%k;
+            freq[r]++;
+        }
+        if (freq[0] % 2 != 0) return false;
+        for (int i = 1; i < k; i++) {
+            if (freq[i] != freq[k - i]) {
+                return false;
             }
         }
-        if(count >= arr.length/2){
-            return true;
-        }
-        return false;
+        return true;
     }
 }
