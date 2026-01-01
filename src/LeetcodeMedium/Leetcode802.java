6175ec7bae422cacab7324c273f88626767d5397
Praveen Verma <knowitspraveeno@gmail.com>
2026-01-01 19:58:28 +0530

    Topological Sort on reverse edged graph

---
+++ src/LeetcodeMedium/Leetcode802.java
@@ -0,0 +1,55 @@
+package LeetcodeMedium;
+
+import java.util.*;
+
+public class Leetcode802 {
+    public static void main(String[] args) {
+        int[][] graph = {{1,2},{2,3},{5},{0},{5},{},{},};
+        System.out.println(eventualSafeNodes(graph));
+    }
+    public static List<Integer> eventualSafeNodes(int[][] graph) {
+        int n = graph.length;
+        int[] indegree = new int[n];
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int node : graph[i]) {
+                adj.get(node).add(i);
+                indegree[i]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        boolean[] safe = new boolean[n];
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            safe[node] = true;
+
+            for (int neighbor : adj.get(node)) {
+                // Delete the edge "node -> neighbor".
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.add(neighbor);
+                }
+            }
+        }
+
+        List<Integer> safeNodes = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (safe[i]) {
+                safeNodes.add(i);
+            }
+        }
+        return safeNodes;
+    }
+}
